ff1cf755984ea97de00dd745464b66d59b0408b0
rooshan baber <rooshanbaber07@gmail.com>
2023-10-17 12:08:06 +0500

    push

--- Frontend/src/app/billing/bill/bill.component.ts
+++ Frontend/src/app/billing/bill/bill.component.ts
@@ -3,7 +3,6 @@ import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms'; 
 import { UserdataService } from 'src/app/services/userdata.service';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-bill',
   templateUrl: './bill.component.html',
@@ -13,14 +12,27 @@ export class BillComponent implements OnInit {
   constructor(private userData: UserdataService, private router: Router) {}
 
   productForms: any = new FormArray([]);
-  
-  ngOnInit(): void {
-    
-  }
+
+  ngOnInit(): void {}
   billData = this.userData.getBillData();
-  totalPriceSum = this.billData.reduce((sum: any, data: { totalPrice: any; }) => sum + data.totalPrice, 0);
+  totalPriceSum = this.billData.reduce(
+    (sum: any, data: { totalPrice: any }) => sum + data.totalPrice,
+    0
+  );
 
   printPage() {
-    window.print();
+    // window.print();
+    const divToPrint = document.getElementById('print-index-invoice');
+    const newWin = window.open('', 'Print-Window');
+    newWin?.document.open();
+    newWin?.document.write(
+      '<html><link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" media="print"/><body onload="window.print()">' +
+        divToPrint?.innerHTML +
+        '</body></html>'
+    );
+    newWin?.document.close();
+    setTimeout(function () {
+      newWin?.close();
+    }, 10);
   }
 }
